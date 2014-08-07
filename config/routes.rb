15b4d7d230920756e66d8f3fdeeef42e40c810f1
Liam Speirs <liamspeirs1@hotmail.com>
2014-08-07 18:25:46 -0400

    Fix?????

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	root_path -> '/'
+	root_url  -> 'http://localhost:3000/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
